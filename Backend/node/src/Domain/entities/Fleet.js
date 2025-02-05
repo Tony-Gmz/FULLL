e786717ab5cbfbab55388843db87047affafac9a
MonPetitMonde <156435112+MonPetitMonde@users.noreply.github.com>
2025-02-05 16:39:15 +0100

    Add function to check vehicle existance in the fleet and add vehicle to the fleet

--- Backend/node/src/Domain/entities/Fleet.js
+++ Backend/node/src/Domain/entities/Fleet.js
@@ -3,6 +3,17 @@ class Fleet {
         this.id = id;
         this.vehicles = new Set();
     }
+
+    addVehicle(vehiclePlateNumber) {
+        if (this.vehicles.has(vehiclePlateNumber)) {
+            throw new Error(`Vehicle ${vehiclePlateNumber} already exists in the fleet`);
+        }
+        this.vehicles.add(vehiclePlateNumber);
+    }
+
+    hasVehicle(vehiclePlateNumber) {
+        return this.vehicles.has(vehiclePlateNumber);
+    }
 }
 
 module.exports = Fleet;
